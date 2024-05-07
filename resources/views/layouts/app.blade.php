b2fd5e8b70d37665898650b135068f0a74858a7d
Cxrvers <sammycarvell@gmail.com>
2024-05-07 20:43:46 +0100

    Blade Template
    Created a Blade Template for the Homepage

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="{{asset('/css/app.css') }}" rel="stylesheet" />
+        <title>@yield('title', 'Online Store')</title>
+    </head>
+<body>
+    <!-- header -->
+    <nav class = "navbar navbar-expand-lg navbar-dark bg-secondary py-4">
+        <div class="container">
+            <a class="navbar-brand" href="#">Online Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active" href="#">Home</a>
+                    <a class="nav-link active" href="#">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+<header class="masthead bg-primary text-white text-center py-4">
+    <div class="container d-flex align-items-center flex-column">
+        <h2>@yield('subtitle', 'A Laravel Online Store')</h2>
+    </div>
+</header>
+<!-- header -->
+<div class="container my-4">
+    @yield('content')
+</div>
+
+<!--footer -->
+<div class="copyright py-4 text-center text-white">
+    <div class="container">
+        <small>
+            Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank" href="https://github.com/cxrvers">Sam Carvell</a> - <b>Also Sam</b>
+        </small>
+    </div>
+</div>
+<!-- footer -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
