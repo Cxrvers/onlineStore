b5e0e79391a069b542f6794aff17f2ba4b5ee88a
Cxrvers <sammycarvell@gmail.com>
2024-05-24 11:53:42 +0100

    created shopping cart

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,94 +6,119 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    /**
-     * PRODUCT ATTRIBUTES
-     * $this->attributes['id] - int - contains the product primary key (id)
-     * $this->attributes['name'] - string - contains the product name
-     * $this->attributes['description'] - string - contains the product description
-     * $this->attributes['image'] - string - contains the product image
-     * $this->attributes['price'] - int - contains the product price
-     * $this->attributes['created_at'] - timestamp - contains the product creation date
-     * $this->attributes['updated_at'] - timestamp - contains the product update date
-     */
+   /**
+    * PRODUCT ATTRIBUTES
+    * $this->attributes['id] - int - contains the product primary key (id)
+    * $this->attributes['name'] - string - contains the product name
+    * $this->attributes['description'] - string - contains the product description
+    * $this->attributes['image'] - string - contains the product image
+    * $this->attributes['price'] - int - contains the product price
+    * $this->attributes['created_at'] - timestamp - contains the product creation date
+    * $this->attributes['updated_at'] - timestamp - contains the product update date
+    * $this->items - Item[] - contains the associated items
+    */
 
-     public static function validate($request)
-     {
+   public static function validate($request)
+   {
       $request->validate([
          "name" => "required|max:255",
          "description" => "required",
          "price" => "required|numeric|gt:0",
          'image' => 'image',
       ]);
-     }
+   }
 
-     public function getId()
-     {
-        return $this->attributes['id'];
-     }
-     
-     public function setId($id)
-     {
-        $this->attributes['id'] = $id;
-     }
+   public static function sumPricesByQuantities($products, $productsInSession)
+   {
+      $total = 0;
+      foreach($products as $product){
+         $total = $total + ($product->getPrice()*$productsInSession[$product->getId()]);
+      }
+      return $total;
+   }
 
-     public function getName()
-     {
-        return $this->attributes['name'];
-     }
+   public function getId()
+   {
+      return $this->attributes['id'];
+   }
 
-     public function setName($name)
-     {
-        $this->attributes['name'] = $name;
-     }
+   public function setId($id)
+   {
+      $this->attributes['id'] = $id;
+   }
 
-     public function getDescription()
-     {
-        return $this->attributes['description'];
-     }
+   public function getName()
+   {
+      return $this->attributes['name'];
+   }
 
-     public function setDescription($description)
-     {
-        $this->attributes['description'] = $description;
-     }
+   public function setName($name)
+   {
+      $this->attributes['name'] = $name;
+   }
 
-     public function getImage()
-     {
-        return $this->attributes['image'];
-     }
+   public function getDescription()
+   {
+      return $this->attributes['description'];
+   }
 
-     public function setImage($image)
-     {
-        $this->attributes['image'] = $image;
-     }
+   public function setDescription($description)
+   {
+      $this->attributes['description'] = $description;
+   }
 
-     public function getPrice()
-     {
-        return $this->attributes['price'];
-     }
+   public function getImage()
+   {
+      return $this->attributes['image'];
+   }
 
-     public function setPrice($price)
-     {
-        $this->attributes['price'] = $price;
-     }
+   public function setImage($image)
+   {
+      $this->attributes['image'] = $image;
+   }
 
-     public function getCreatedAt()
-     {
-        return $this->attributes['created_at'];
-     }
+   public function getPrice()
+   {
+      return $this->attributes['price'];
+   }
 
-     public function setCreatedAt($createdAt)
-     {
-        $this->attributes['created_at'] = $createdAt;
-     }
+   public function setPrice($price)
+   {
+      $this->attributes['price'] = $price;
+   }
 
-     public function getUpdatedAt()
-     {
-        return $this->attributes['updated_at'];
-     }
+   public function getCreatedAt()
+   {
+      return $this->attributes['created_at'];
+   }
 
-     public function setUpdatedAt($updated_at)
-     {
-        $this->attributes['updated_at'] = $updated_at;
-     }
+   public function setCreatedAt($createdAt)
+   {
+      $this->attributes['created_at'] = $createdAt;
+   }
+
+   public function getUpdatedAt()
+   {
+      return $this->attributes['updated_at'];
+   }
+
+   public function setUpdatedAt($updated_at)
+   {
+      $this->attributes['updated_at'] = $updated_at;
+   }
+
+   public function items()
+   {
+      return $this->hasMany(Item::class);
+   }
+
+   public function getItems()
+   {
+      return $this->items;
+   }
+
+   public function setItems($items)
+   {
+      $this->items = $items;
+   }
 }
